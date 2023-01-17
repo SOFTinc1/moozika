eff1dd88deb0f9bc1c670a52c5c7404fec80bf1d
SOFTinc <idowuelijah72@gmail.com.com>
2023-01-17 12:17:55 +0100

    first commit

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,6 @@
 import { HomepageContainer, Basic, Flex, HomeItems } from "./homepage.styles";
 import Banner from "../../components/banner/banner.component";
-// import Charts from "../../components/charts/charts.component";
+import Charts from "../../components/charts/charts.component";
 // import Test from "../../components/test/banner.component";
 import Header from "../../components/header/header.component";
 import SideTab from "../../components/side-tab/side-tab.component";
